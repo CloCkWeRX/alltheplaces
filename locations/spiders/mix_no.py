cd6d622a64915632d61e08ab77175668009b3284
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-17 03:10:02 +0000

    Add MIX (NO)

---
+++ locations/spiders/mix_no.py
@@ -0,0 +1,8 @@
+from locations.storefinders.sylinder import SylinderSpider
+
+
+class MixNoSpider(SylinderSpider):
+    name = "mix_no"
+    item_attributes = {"brand": "MIX", "brand_wikidata": "Q56404240"}
+    app_key = "1410"
+    base_url = "https://mix.no/#" # Note: Doesn't use the same storefinder
