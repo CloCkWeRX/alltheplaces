dd8db7b4eb1f967d19b12177b43b121bd71bd0cf
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-03-17 03:10:07 +0000

    Add MIX (NO)

--- locations/spiders/mix_no.py
+++ locations/spiders/mix_no.py
@@ -5,4 +5,4 @@ class MixNoSpider(SylinderSpider):
     name = "mix_no"
     item_attributes = {"brand": "MIX", "brand_wikidata": "Q56404240"}
     app_key = "1410"
-    base_url = "https://mix.no/#" # Note: Doesn't use the same storefinder
+    base_url = "https://mix.no/#"  # Note: Doesn't use the same storefinder
