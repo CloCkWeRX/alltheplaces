40313d89309c0759831ebbefb8b37996c604c733
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-15 12:13:30 +0000

    Add a terrible python script for generating a bunch of spiders by running spiders

---
+++ run.py
@@ -0,0 +1,50 @@
+import subprocess
+import os.path
+
+parts = {}
+files = ["xaa", "xab", "xac", "xad", "xae", "xae", "xaf", "xag", "xah", "xai", "xaj", "xak", "xal"]
+
+for path in files:
+    print(path)
+    with open("../" + path) as file:
+        for line in file:
+            bits = line.rstrip().split("/")
+            if len(bits) > 2:
+                key = "/".join([bits[0], bits[1], bits[2]])
+                if key in parts:
+                    parts[key].append(line)
+                else:
+                    spider_name = bits[2].replace(".", "_")
+                    if os.path.isfile("locations/spiders/sf_1_{}.py".format(spider_name)):
+                        continue
+                    if os.path.isfile("locations/spiders/sf_2_{}.py".format(spider_name)):
+                        continue
+                    if os.path.isfile("locations/spiders/web_data_commons_{}.py".format(spider_name)):
+                        continue
+
+                    parts[key] = []
+
+                    print(line)
+                    result = subprocess.run(["pipenv run scrapy sd " + line], capture_output=True, shell=True)
+                    lines = result.stdout.splitlines()
+                    if len(lines) > 2:
+                        print(lines)
+                        auto_detect = subprocess.run(["pipenv run scrapy sf " + line], capture_output=True, shell=True).stdout.splitlines()
+                        auto_detect2 = subprocess.run(["pipenv run scrapy sf " + key], capture_output=True, shell=True).stdout.splitlines()
+
+                        if (len(auto_detect) > 1):
+                            subprocess.run(["""echo '{}' > locations/spiders/sf_1_{}.py""".format("\n".join(auto_detect), spider_name)], shell=True)
+
+                        if (len(auto_detect2) > 1):
+                            subprocess.run(["""echo '{}' > locations/spiders/sf_2_{}.py""".format("\n".join(auto_detect2), spider_name)], shell=True)
+                        subprocess.run(["""echo 'from scrapy.spiders import SitemapSpider
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class WebCommons{}Spider(SitemapSpider, StructuredDataSpider):
+    name = "web_commons_{}"
+    sitemap_urls = ["{}/robots.txt"]
+    wanted_types = ["LocalBusiness"]
+' > locations/spiders/web_commons_{}.py
+""".format(spider_name, spider_name, key, spider_name)], shell=True)
