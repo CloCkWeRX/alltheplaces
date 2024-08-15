6ca93680b0d15975c12adc22b3e4601811aa8515
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-08-15 13:01:01 +0000

    Add more

--- run.py
+++ run.py
@@ -2,8 +2,9 @@ import os.path
 import subprocess
 
 parts = {}
-files = ["xaa", "xab", "xac", "xad", "xae", "xae", "xaf", "xag", "xah", "xai", "xaj", "xak", "xal"]
+files = [ "xaa", "xab", "xac", "xad", "xae", "xae", "xaf", "xag", "xah", "xai", "xaj", "xak", "xal"]
 
+print("Loading into memory")
 for path in files:
     print(path)
     with open("../" + path) as file:
@@ -15,48 +16,62 @@ for path in files:
                     parts[key].append(line)
                 else:
                     spider_name = bits[2].replace(".", "_")
-                    if os.path.isfile("locations/spiders/sf_1_{}.py".format(spider_name)):
+                    if os.path.isfile("./locations/spiders/sf_1_{}.py".format(spider_name)):
                         continue
-                    if os.path.isfile("locations/spiders/sf_2_{}.py".format(spider_name)):
+                    elif os.path.isfile("./locations/spiders/sf_2_{}.py".format(spider_name)):
                         continue
-                    if os.path.isfile("locations/spiders/web_data_commons_{}.py".format(spider_name)):
+                    elif os.path.isfile("./locations/spiders/web_data_commons_{}.py".format(spider_name)):
                         continue
+                    else:
+                        parts[key] = []
 
-                    parts[key] = []
+for key, urls in parts.items():
+    for line in urls:
+        bits = line.rstrip().split("/")
+        spider_name = bits[2].replace(".", "_")
 
-                    print(line)
-                    result = subprocess.run(["pipenv run scrapy sd " + line], capture_output=True, shell=True)
-                    lines = result.stdout.splitlines()
-                    if len(lines) > 2:
-                        print(lines)
-                        auto_detect = subprocess.run(
-                            ["pipenv run scrapy sf " + line], capture_output=True, shell=True
-                        ).stdout.splitlines()
-                        auto_detect2 = subprocess.run(
-                            ["pipenv run scrapy sf " + key], capture_output=True, shell=True
-                        ).stdout.splitlines()
+        if os.path.isfile("./locations/spiders/sf_1_{}.py".format(spider_name)):
+            continue
+        elif os.path.isfile("./locations/spiders/sf_2_{}.py".format(spider_name)):
+            continue
+        elif os.path.isfile("./locations/spiders/web_data_commons_{}.py".format(spider_name)):
+            continue
+        else:
+            print(line)
+            result = subprocess.run(["pipenv run scrapy sd " + line], capture_output=True, shell=True)
+            lines = result.stdout.splitlines()
+            if len(lines) <= 2:
+                subprocess.run(["touch ./locations/spiders/web_data_commons_{}.py".format(spider_name)], shell=True)
+            elif len(lines) > 2:
+                print(lines)
+                auto_detect = subprocess.run(
+                    ["pipenv run scrapy sf " + line], capture_output=True, shell=True
+                ).stdout.splitlines()
+                auto_detect2 = subprocess.run(
+                    ["pipenv run scrapy sf " + key], capture_output=True, shell=True
+                ).stdout.splitlines()
 
-                        if len(auto_detect) > 1:
-                            subprocess.run(
-                                [
-                                    """echo '{}' > locations/spiders/sf_1_{}.py""".format(
-                                        "\n".join(auto_detect), spider_name
-                                    )
-                                ],
-                                shell=True,
+                if len(auto_detect) > 1:
+                    subprocess.run(
+                        [
+                            """echo '{}' > locations/spiders/sf_1_{}.py""".format(
+                                "\n".join(auto_detect), spider_name
                             )
+                        ],
+                        shell=True,
+                    )
 
-                        if len(auto_detect2) > 1:
-                            subprocess.run(
-                                [
-                                    """echo '{}' > locations/spiders/sf_2_{}.py""".format(
-                                        "\n".join(auto_detect2), spider_name
-                                    )
-                                ],
-                                shell=True,
+                if len(auto_detect2) > 1:
+                    subprocess.run(
+                        [
+                            """echo '{}' > locations/spiders/sf_2_{}.py""".format(
+                                "\n".join(auto_detect2), spider_name
                             )
-                        subprocess.run(
-                            [
+                        ],
+                        shell=True,
+                    )
+                subprocess.run(
+                                [
                                 """echo 'from scrapy.spiders import SitemapSpider
 
 from locations.structured_data_spider import StructuredDataSpider
@@ -68,8 +83,8 @@ class WebCommons{}Spider(SitemapSpider, StructuredDataSpider):
     wanted_types = ["LocalBusiness"]
 ' > locations/spiders/web_commons_{}.py
 """.format(
-                                    spider_name, spider_name, key, spider_name
+                                            spider_name, spider_name, key, spider_name
+                                        )
+                                    ],
+                                    shell=True,
                                 )
-                            ],
-                            shell=True,
-                        )
