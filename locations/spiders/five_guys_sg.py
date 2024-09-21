9c93249f2e048ffcb550ec287115dd9016ed4d0a
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-21 10:04:51 +0000

    Remove dead spider

--- locations/spiders/five_guys_sg.py
+++
@@ -1,7 +0,0 @@
-from locations.spiders.five_guys_au import FiveGuysAUSpider
-
-
-class FiveGuysSGSpider(FiveGuysAUSpider):
-    name = "five_guys_sg"
-    experience_key = "search-backend-sg"
-    locale = "en-SG"
